e1fce4350e051a8af0d91a382dcc833019724c0d
Abiorh001 <abiolaadedayo1993@gmail.com>
2023-05-09 09:05:56 -0700

    api advanced

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,23 +4,26 @@
 import requests
 after = None
 
+
 def recurse(subreddit, hot_list=[]):
     """a functiion to perform an api recursively"""
 
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {'User-Agent': 'chrome'}
     global after
-    params = {"after" : after}
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    params = {"after": after}
+    response = requests.get(url, headers=headers, allow_redirects=False,
+                            params=params)
 
     if response.status_code == 200:
         data = response.json()
         posts = data.get("data").get("children")
-
-        if data['data']['after'] is not None:
+        next_ = data.get("data").get("after")
+        if next_ is not None:
+            after = next_
             recurse(subreddit, hot_list)
         for post in posts:
             hot_list.append(post['data']['title'])
         return hot_list
     else:
-        return None
+        return (None)
