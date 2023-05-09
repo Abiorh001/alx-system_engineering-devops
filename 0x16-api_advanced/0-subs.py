447de122dd7bf0e680f7fa1b2014bf8a7d3d8940
Abiorh001 <abiolaadedayo1993@gmail.com>
2023-05-09 02:48:40 -0700

    api advanced

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" a script to fect number of subscribers"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ a function to perform the task"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'My User Agent'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if (response.status_code == 200):
+        data = response.json()
+        subs = data.get('data').get('subscribers')
+        print(subs)
+    else:
+        return 0
